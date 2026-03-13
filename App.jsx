7ac00804253c26e85293e5bc92d87533d49ad8e6
ShivamRana1270 <shivam.rana2291@gmail.com>
2026-03-13 21:06:46 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@
 // import './App.css'
 // import SearchIcon from './search.svg'
 
-// const API_URL= "http://www.omdbapi.com/?apikey=4bbf4965"
+// const API_URL= "https://www.omdbapi.com/?apikey=4bbf4965"
 
 
 // const movie=
